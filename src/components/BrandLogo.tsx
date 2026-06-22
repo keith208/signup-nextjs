54778b72e59142a944d532c79598e2054b5b2508
keith208 <keith@remingtoncole.com>
2026-06-22 11:10:32 -0500

    Add BrandLogo component with customizable app name

---
+++ src/components/BrandLogo.tsx
@@ -0,0 +1,32 @@
+```typescript
+import React from "react";
+import styles from "./BrandLogo.module.css";
+
+interface BrandLogoProps {
+  appName?: string;
+}
+
+export function BrandLogo({ appName = "Platform" }: BrandLogoProps) {
+  return (
+    <div className={styles.brand}>
+      <div className={styles.icon}>
+        <svg
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="white"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="M12 2L2 7l10 5 10-5-10-5z" />
+          <path d="M2 17l10 5 10-5" />
+          <path d="M2 12l10 5 10-5" />
+        </svg>
+      </div>
+      <span className={styles.name}>{appName}</span>
+    </div>
+  );
+}
+```
